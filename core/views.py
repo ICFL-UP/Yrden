54ddacf3085091459dca4f92b4328f5bdd34370c
Tjaart <35308889+Tjaart-Booyens@users.noreply.github.com>
2021-08-02 20:26:07 +0200

    Closes #3 (#11)
    * Feat: Added subprocess calls to plugins attached to the system
    
    It creates a venv and install the plugin requirements for the plugin and can then expand on the main functionality of the plugin.
    Removed plugin folders from git and will create an examples directory later.
    
    * Feat: Updated PR for closing
    
    Removed code not needed anymore in order to close the PR.

--- core/views.py
+++ core/views.py
@@ -1,12 +1,9 @@
 from django.db.models.query import QuerySet
+from django.http.response import HttpResponse
 from django.views import generic
 
 from .models import Plugin
 from .forms import PluginCreateForm
-
-from .plugin.plugin_mount import PluginMount
-
-pm = PluginMount()
 
 app_name = 'core'
 
@@ -31,3 +28,29 @@ class PluginCreateView(generic.CreateView):
     template_name_suffix = '_create_form'
     model = Plugin
     form_class = PluginCreateForm
+
+    def form_valid(self, form: PluginCreateForm) -> HttpResponse:
+        # TODO: handle logic for installing the plugin
+        return super().form_valid(form)
+
+
+# This should be moved out (only using this for testing until the crontabs are set up fully)
+# =============================================
+
+def walk_plugins(plugin: Plugin):
+    import os
+    import subprocess
+
+    dir = 'core/plugin'
+    for fname in os.listdir(dir):
+        main = dir + os.sep + fname + os.sep + 'main.py'
+        if fname.lower() == str(plugin.name).lower() and os.path.isfile(main):
+            process = subprocess.run(['python', main], check=True, stdout=subprocess.PIPE)
+            output = process.stdout
+
+
+qs = Plugin.objects.all()
+for plugin in qs:
+    walk_plugins(plugin)
+
+# =============================================
